2fc12d1dfe04ab750be31fc0fbef4f046343191b
Antonin Amand <antonin.amand@feedback20.com>
2008-10-12 20:03:57 +0200

    adding README and example

---
+++ animated_gif_example.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+require 'ffmpeg'
+require 'rmagick'
+
+video = FFMPEG::InputFormat.new('spec/data/alligator.mp4')
+stream = video.first_video_stream
+stream.seek 12
+
+i = 0
+image_list = Magick::ImageList.new
+
+# pts is presentation timestamp
+# dts is decoding timestamp
+stream.decode_frame do |frame, pts, dts|
+  i += 1
+  # stop when decoding timestamp (~position) reach 18
+  break if dts > 18
+  # decode 1 frame for 5
+  next unless i % 5 == 0
+  image_list.from_blob(frame.to_ppm)
+end
+
+puts i 
+image_list.delay = 20
+image_list.write('animated.gif')
